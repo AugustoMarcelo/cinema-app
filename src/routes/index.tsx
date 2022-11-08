845690719bcf3f4837656be205493e4e038c8ee8
Marcelo <mrclgst10@gmail.com>
2022-11-08 16:39:23 -0300

    add box around navigator to avoid flashes and navigating

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { Box } from 'native-base';
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="dark.800">
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </Box>
   );
 }
