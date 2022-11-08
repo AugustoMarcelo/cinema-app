91776bcfcbfd3b818888f4f05d025807aa6fa1de
Marcelo <mrclgst10@gmail.com>
2022-11-08 16:37:58 -0300

    refactor screen to create components

--- src/screens/Catalog.tsx
+++ src/screens/Catalog.tsx
@@ -1,15 +1,30 @@
-import { Entypo, Ionicons } from '@expo/vector-icons';
-import { Button, FlatList, Icon, Image, Text, View, VStack } from 'native-base';
-import InsetShadow from 'react-native-inset-shadow';
+import { FlatList, View, VStack } from 'native-base';
 import { Header } from '../components/Header';
 
 import Ellipsis1 from '../assets/ellipsis1.svg';
 import Ellipsis2 from '../assets/ellipsis2.svg';
 import Ellipsis3 from '../assets/ellipsis3.svg';
 
+import { useNavigation } from '@react-navigation/native';
+import { useCallback, useRef, useState } from 'react';
 import { data } from '../../data.json';
+import { Button } from '../components/Button';
+import { MovieCard } from '../components/MovieCard';
 
 export function Catalog() {
+  const [visibleItemId, setVisibleItemId] = useState<string>('');
+
+  const { navigate } = useNavigation();
+
+  const onViewableItemsChanged = useCallback(({ changed }) => {
+    setVisibleItemId((oldState) => {
+      if (oldState !== changed[0].item.id) return changed[0].item.id;
+      return oldState;
+    });
+  }, []);
+
+  const viewabilityConfig = useRef({ viewAreaCoveragePercentThreshold: 95 });
+
   return (
     <VStack flex={1} bgColor="dark.800" position="relative">
       <Header />
@@ -39,110 +54,15 @@ export function Catalog() {
           decelerationRate={0}
           data={data}
           keyExtractor={(item) => String(item.id)}
-          renderItem={({ item }) => (
-            <View width={72} mx={5}>
-              <View alignItems="center">
-                <Image
-                  source={{ uri: item.cover_url }}
-                  rounded="2xl"
-                  alt="Movie cover URL"
-                  mb={10}
-                  style={{
-                    height: 336,
-                    width: 272,
-                  }}
-                />
-                <Text
-                  color="gray.20"
-                  fontSize="lg"
-                  fontFamily="heading"
-                  textTransform="uppercase"
-                  textAlign="center"
-                >
-                  {item.title}
-                </Text>
-                <Text
-                  color="#676080"
-                  fontSize="sm"
-                  fontFamily="regular"
-                  textAlign="center"
-                  mt={2}
-                  mb={4}
-                >
-                  {item.directed_by}
-                </Text>
-                <View flexDirection="row">
-                  {Array.from({ length: 5 }).map((_, index: number) => (
-                    <Icon
-                      key={index}
-                      as={<Ionicons name="star" />}
-                      color="yellow.500"
-                      size="lg"
-                      mr={2}
-                    />
-                  ))}
-                </View>
-              </View>
-            </View>
-          )}
+          onViewableItemsChanged={onViewableItemsChanged}
+          viewabilityConfig={viewabilityConfig.current}
+          renderItem={({ item }) => <MovieCard data={item} />}
         />
 
-        <View
+        <Button
           mt={5}
-          rounded="full"
-          height={12}
-          style={{
-            shadowColor: 'rgba(151,71,255,0.5)',
-            shadowRadius: 40,
-            shadowOffset: {
-              width: 0,
-              height: 12,
-            },
-          }}
-          shadow="9"
-        >
-          <InsetShadow
-            shadowRadius={40}
-            shadowOffset={12}
-            shadowColor="rgba(255, 255, 255, 0.6)"
-            right={false}
-            left={false}
-            bottom={false}
-            containerStyle={{
-              borderRadius: 25,
-            }}
-          >
-            <Button
-              leftIcon={
-                <Icon as={<Entypo name="ticket" />} color="white" size={6} />
-              }
-              bgColor="blue.800"
-              py={3}
-              px={12}
-              rounded="full"
-              style={{
-                shadowColor: '#fff',
-                shadowRadius: 40,
-                shadowOffset: {
-                  width: 0,
-                  height: 12,
-                },
-                shadowOpacity: 1,
-              }}
-              shadow="9"
-            >
-              <Text
-                color="white"
-                textTransform="uppercase"
-                fontFamily="heading"
-                fontSize="16"
-                ml={3}
-              >
-                Comprar
-              </Text>
-            </Button>
-          </InsetShadow>
-        </View>
+          handleClick={() => navigate('MovieInfo', { id: visibleItemId })}
+        />
       </View>
     </VStack>
   );
