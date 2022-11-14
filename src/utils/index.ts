f3ee85a268389090729df4b7cf5fb7d80523017e
Marcelo <mrclgst10@gmail.com>
2022-11-14 13:20:58 -0300

    refactor function to generate seats status

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -21,100 +21,103 @@ export interface SelectTime {
   isSelected: boolean;
 }
 
+function generateRandomStatus() {
+  const value = Math.ceil(Math.random() * 10);
+
+  if (value <= 5) return 'free';
+  return 'occuped';
+}
+
 export const getInitializedSeatsData = (): SeatsRow[] => {
   return [
     {
       leftSeats: Array.from<SeatProps>({ length: 3 }).map((_, index) => ({
         number: index + 1,
-        status: 'free',
+        status: generateRandomStatus(),
       })),
       rowLetter: 'A',
       rightSeats: Array.from<SeatProps>({ length: 3 }).map((_, index) => ({
         number: index + 1 + 3,
-        status: 'free',
+        status: generateRandomStatus(),
       })),
     },
     {
       leftSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => ({
         number: index + 1,
-        status: 'free',
+        status: generateRandomStatus(),
       })),
       rowLetter: 'B',
       rightSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => ({
         number: index + 1 + 4,
-        status: index + 1 + 4 <= 7 ? 'occuped' : 'free',
+        status: generateRandomStatus(),
       })),
     },
     {
       leftSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => ({
         number: index + 1,
-        status: index + 1 < 4 ? 'free' : 'occuped',
+        status: generateRandomStatus(),
       })),
       rowLetter: 'C',
       rightSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => ({
         number: index + 1 + 4,
-        status: index + 1 + 4 === 7 ? 'occuped' : 'free',
+        status: generateRandomStatus(),
       })),
     },
     {
       leftSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => ({
         number: index + 1,
-        status: index + 1 < 3 ? 'free' : 'occuped',
+        status: generateRandomStatus(),
       })),
       rowLetter: 'D',
       rightSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => {
-        const seats = {
-          6: 'selected',
-          8: 'occuped',
-        };
         return {
           number: index + 1 + 4,
-          status: seats[index + 1 + 4] ?? 'free',
+          status: generateRandomStatus(),
         };
       }),
     },
     {
       leftSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => ({
         number: index + 1,
-        status: [1, 4].includes(index + 1) ? 'occuped' : 'free',
+        status: generateRandomStatus(),
       })),
       rowLetter: 'E',
       rightSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => ({
         number: index + 1 + 4,
-        status: index + 1 + 4 === 7 ? 'occuped' : 'free',
+        status: generateRandomStatus(),
       })),
     },
     {
       leftSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => ({
         number: index + 1,
-        status: 'occuped',
+        status: generateRandomStatus(),
       })),
       rowLetter: 'F',
       rightSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => ({
         number: index + 1 + 4,
-        status: index + 1 + 4 < 8 ? 'occuped' : 'free',
+        status: generateRandomStatus(),
       })),
     },
     {
       leftSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => ({
         number: index + 1,
-        status: index + 1 > 1 ? 'occuped' : 'free',
+        status: generateRandomStatus(),
       })),
       rowLetter: 'G',
       rightSeats: Array.from<SeatProps>({ length: 4 }).map((_, index) => ({
         number: index + 1 + 4,
-        status: index + 1 + 4 === 5 ? 'occuped' : 'free',
+        status: generateRandomStatus(),
       })),
     },
     {
       leftSeats: Array.from<SeatProps>({ length: 3 }).map((_, index) => ({
         number: index + 1,
-        status: index + 1 > 1 ? 'occuped' : 'free',
+        status: generateRandomStatus(),
       })),
       rowLetter: 'H',
       rightSeats: Array.from<SeatProps>({ length: 3 }).map((_, index) => ({
         number: index + 1 + 4,
-        status: 'free',
+        status: generateRandomStatus(),
       })),
     },
   ];
@@ -155,7 +158,7 @@ export const getInitializedTimesData = (): SelectTime[] => {
   });
 };
 
-export const getHashCodeFrom = (value: string): number => {
+export const generateHashCodeFrom = (value: string): number => {
   const sanitizedValue = value.replace(/[^a-zA-Z0-9]/g, '');
   let hash = 0;
   let chr: number;
@@ -167,5 +170,5 @@ export const getHashCodeFrom = (value: string): number => {
     hash = (hash << 5) - hash + chr;
     hash |= 0; // Convert to 16bit integer
   }
-  return hash;
+  return Math.abs(hash);
 };
